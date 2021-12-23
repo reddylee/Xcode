46f12f2f5008d593e4e4d5bf174a50c5f37675db
Reddy Lee <reddylee@gmail.com>
2021-12-23 22:42:51 +0000

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 秦耕读 on 23/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/XcodeTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeTutorialApp.swift
+//  Shared
+//
+//  Created by 秦耕读 on 23/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct XcodeTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
